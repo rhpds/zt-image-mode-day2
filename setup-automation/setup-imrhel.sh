194a12f766d28333d4973577a0f0c4e18c1a83c9
Matt Micene (RHT) <mmicene@redhat.com>
2025-08-18 16:52:26 -0400

    Revert "try the reinstall via ansible with updated system-reinstall-bootc params"
    This reverts commit 6d6592aea7eb2474467e7a92f8d20facd85afcb4.

--- setup-automation/setup-imrhel.sh
+++ setup-automation/setup-imrhel.sh
@@ -2,13 +2,6 @@
 
 
 ## Convert the system to Image Mode
-# system-reinstall-bootc quay.io/toharris/rhel-bootc:summit-2025
-# Found only one user (root) with 1 SSH authorized keys.
-# Would you like to import its SSH authorized keys
-# into the root user on the new bootc system? yes
-
-# Going to run command "podman" "run" "--privileged" "--pid=host" "--user=root:root" "-v" "/var/lib/containers:/var/lib/containers" "-v" "/dev:/dev" "--security-opt" "label=type:unconfined_t" "-v" "/:/target" "-v" "/tmp/.tmp08wosc:/bootc_authorized_ssh_keys/root" "quay.io/toharris/rhel-bootc:summit-2025" "bootc" "install" "to-existing-root" "--acknowledge-destructive" "--root-ssh-authorized-keys" "/bootc_authorized_ssh_keys/root"
 # podman run --rm --privileged -v /dev:/dev -v /var/lib/containers:/var/lib/containers -v /:/target --pid=host --security-opt label=type:unconfined_t quay.io/toharris/rhel-bootc:summit-2025 bootc install to-existing-root --root-ssh-authorized-keys /target/root/.ssh/authorized_keys --acknowledge-destructive
-podman run --privileged --pid=host --user=root:root -v /var/lib/containers:/var/lib/containers -v /dev:/dev --security-opt label=type:unconfined_t -v /:/target -v /tmp/.tmp08wosc:/bootc_authorized_ssh_keys/root quay.io/toharris/rhel-bootc:summit-2025 bootc install to-existing-root --acknowledge-destructive --root-ssh-authorized-keys /bootc_authorized_ssh_keys/root
 
 echo "DONE" >> /root/job.log
